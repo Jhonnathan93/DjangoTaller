cdbdd915dd07ca1c1f07aa7336b6b477ec787257
jhonnathan ocampo <jhonnathanocampo118@gmail.com>
2023-08-08 13:48:51 -0500

    Formulario de búsqueda

--- movie/views.py
+++ movie/views.py
@@ -9,11 +9,12 @@ def home(request):
     # return render(request, 'home.html')
     # return render(request, 'home.html', {'name':'Jhonnathan Stiven Ocampo'}) 
     searchTerm = request.GET.get('searchMovie')
-    if searchTerm:
-        movies = Movie.objects.filter(title__icontains=searchTerm)
-    else:
-        movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
+    return render(request, 'home.html', {'searchTerm':searchTerm})
+    # if searchTerm:
+        # movies = Movie.objects.filter(title__icontains=searchTerm)
+    # else:
+        # movies = Movie.objects.all()
+    # return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
 
 def about(request):
     return render(request, 'about.html')
